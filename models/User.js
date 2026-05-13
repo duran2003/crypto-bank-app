4e969be5c50dda2013ff7e8c31068e780e4e11ae
duran2003 <duranderson2025@gmail.com>
2026-05-12 21:31:17 -0400

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,31 @@
+const { DataTypes } = require("sequelize");
 
+module.exports = (sequelize) => {
+    return sequelize.define("User", {
+        userid: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        username: {
+            type: DataTypes.STRING
+        },
+        email: {
+            type: DataTypes.STRING
+        },
+        zipcode: {
+            type: DataTypes.STRING
+        },
+        type: {
+            type: DataTypes.STRING,
+            defaultValue: "Customer"
+        },
+        walletAddress: {
+            type: DataTypes.STRING
+        }
+    });
+};
