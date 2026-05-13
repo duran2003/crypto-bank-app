f45af9b8eb79d9ed94ca3f537d03b89d2a964304
duran2003 <duranderson2025@gmail.com>
2026-05-12 21:32:47 -0400

    Update Investment.js

--- models/Investment.js
+++ models/Investment.js
@@ -1,1 +1,26 @@
+const { DataTypes } = require("sequelize");
 
+module.exports = (sequelize) => {
+    return sequelize.define("Investment", {
+        userid: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        bitcoin: {
+            type: DataTypes.FLOAT,
+            defaultValue: 0
+        },
+        ethereum: {
+            type: DataTypes.FLOAT,
+            defaultValue: 0
+        },
+        solana: {
+            type: DataTypes.FLOAT,
+            defaultValue: 0
+        },
+        dogecoin: {
+            type: DataTypes.FLOAT,
+            defaultValue: 0
+        }
+    });
+};
